379f0763f9a61fcf335320581dabab657f5db297
Benjamin Kay <benjamin@benkay.net>
2022-07-07 15:56:37 -0500

    Commented and streamlined examples

--- example.m
+++ example.m
@@ -1,31 +1,33 @@
 % Generate example/test data for a mixed effects model.
 %
 % Premise: A scientist wants to test if SAT score is correlated with high
-% school GPA.  She has collected data from 120 students attending 3 high
-% schools in St. Louis: Jennings (a public high school plagues by poverty),
+% school GPA.  She has collected data from 2300 students attending 3 high
+% schools in St. Louis: Jennings (a public high school plagued by poverty),
 % Gateway Science Academy (a charter school), and Burroughs (an elite
-% college prep school). Due to data being collected during the COVID-19
-% pandemic, students attended school using Zoom, Google Meet, Microsoft
-% Teams, or in-person.
+% college prep school).
 %
-% GPA ranges from 1.0 to 4.0 with an average of about 3.0.
+% GPA ranges from 1.0 to 4.0 with an average of about 2.5.
 % SAT scores range from 400 (lowest possible score) to 1600 (perfect).
 % Both are (supposed) to be normally distributed.
 
 %% Generate the Data
 
-% Number of students in each school.
+% Number of students in each school, or "cluster."
+% Try playing with having an equal (balanced) vs unequal (unbalanced)
+% number of students in each school.
 n_jennings = 1000;
 n_gateway = 800;
 n_burroughs = 500;
 n = n_jennings + n_gateway + n_burroughs;
 
-% Generate GPAs.
+% Generate GPAs.  Trim GPAs outside the range of possible values.
 gpa = normrnd(2.5,0.5,n,1);
 gpa(gpa < 1) = 1;
 gpa(gpa > 4) = 4;
 
-% Split students into 3 high schools.
+% Encode the three high schools in an n x 3 matrix.  Each high school
+% occupies one column.  Values of 1 indicate a student attends the high
+% school for the corresponding column.  The matrix is zero everywhere else.
 school = zeros(n,3);
 school(1:n_jennings, 1) = 1; % Jennings
 school((n_jennings+1):(n_jennings+n_gateway), 2) = 1; % Gateway
@@ -37,7 +39,8 @@ school((n_jennings+n_gateway+1):n, 3) = 1; % Burroughs
 % 3 = Burroughs
 G = sum(school .* [1,2,3], 2);
 
-% Simulate some overall relationship between SAT score and GPA.
+% Simulate some overall ground-truth relationship between SAT score and
+% GPA.
 intercept = 300;
 slope = 200;
 sat = intercept + gpa .* slope;
@@ -58,6 +61,8 @@ slope = slope + mean(school_slope);
 
 % The GPA of students at Burroughs is inflated.
 % This makes the fixed effects only estimate biased.
+% Try commenting this out and see what happens to the fixed effects only
+% estimate.
 gpa(school(:,3) == 1) = gpa(school(:,3) == 1) + 0.5;
 gpa(gpa > 4) = 4;
 
@@ -71,7 +76,7 @@ sat(sat > 1600) = 1600;
 % Store data in a closure for easy plotting of results.
 plot_clusters = make_plot_fn(sat, gpa, school);
 
-%% Fixed Effects Model Without Clusters
+%% Fixed Effects Only Model (Without Clusters)
 
 model_fixed = struct;
 
@@ -87,15 +92,15 @@ model_fixed.X = X;
 Xpinv = pinv(X);
 model_fixed.B = pinv(X)*Y;
 
-% Find the standard errors.
-model_fixed.SE = sqrt(diag(pinv(X)*pinv(X)'*sum((Y-X*model_fixed.B).^2)/size(Y,1)));
-
-% Compute T-values.
-model_fixed.T = model_fixed.B ./ model_fixed.SE;
-
 % Scatterplot
 plot_clusters(model_fixed.B);
 title('Fixed Effects Only (No Clusters)');
+
+% Find the standard errors.
+model_fixed.SE = sqrt(diag(Xpinv*Xpinv'*sum((Y-X*model_fixed.B).^2)/size(Y,1)));
+
+% Compute T-values.
+model_fixed.T = model_fixed.B ./ model_fixed.SE;
 
 % Compute Standard Error for Fixed Effects Model Using Sandwich Estimator
 resid = model_fixed.Y - model_fixed.X * model_fixed.B;
@@ -106,57 +111,14 @@ clear resid Xpinv
 % using different method.
 comparison_tbl = table(repmat(model_fixed.B', 2, 1), [model_fixed.SE'; sqrt(diag(model_fixed.swe_covB.block))'], 'VariableNames', {'B', 'SE'}, 'RowNames', {'Fixed Only', 'Fixed + Block SwE'});
 
-%% Mixed Effects Model, Random Intercept for School
+%% Mixed Effects Model using Method of Moments
+% With Random Intercept and Centered Slope for School
 
-% The random effect design matrix is just three columns of ones and zeros
-% encoding which school the student goes to.
-Z = school;
-
-% There is only one random effect variance, that of the intercept.
-sigma_ncols = [3];
-
-% Fit the mixed effects model using the method of moments!
-model_int = mom(X, Y, Z, sigma_ncols);
-clear Z sigma_ncols;
-
-% We can extract the standard error from the diagonal of the
-% variance-covariance matrix for B.
-model_int.SE = sqrt(diag(model_int.B_cov));
-model_int.T = model_int.B ./ model_int.SE;
-
-% Scatterplot
-plot_clusters(model_int.B, [model_int.u; 0; 0; 0]);
-title('Random Interecept, MoM');
-
-% Add to comparison table.
-comparison_tbl = [comparison_tbl; table(model_int.B', model_int.SE', 'VariableNames', {'B', 'SE'}, 'RowNames', {'MoM Intercept'})];
-
-%% Mixed Effects Model, Random Slope for School
-
-% The random effect design matrix is three columns containing the GPA for
-% schools in the column, otherwise zero.
-Z = school.*gpa;
-
-% There is only one random effect variance, that of the slope.
-sigma_ncols = [3];
-
-% Fit the mixed effects model using the method of moments!
-model_slope = mom(X, Y, Z, sigma_ncols);
-clear X Z sigma_ncols;
-
-% We can extract the standard error from the diagonal of the
-% variance-covariance matrix for B.
-model_slope.SE = sqrt(diag(model_slope.B_cov));
-model_slope.T = model_slope.B ./ model_slope.SE;
-
-% Scatterplot
-plot_clusters(model_slope.B, [0; 0; 0; model_slope.u]);
-title('Random Slope, MoM');
-
-% Add to comparison table.
-comparison_tbl = [comparison_tbl; table(model_slope.B', model_slope.SE', 'VariableNames', {'B', 'SE'}, 'RowNames', {'MoM Slope'})];
-
-%% Mixed Effects Model, Random Intercept and Centered Slope for School
+% MoM with intercept + slope requires us to center the random effects
+% design matrix, Z, to avoid colinearity when performing Haesman-Elston
+% regression.  For simplicity we will center the whole model.  If you fail
+% to do this you will probably get warnings about negative random effect
+% variance parameters (sigmas).
 
 % Center the slope by subtracting away its mean.
 X_centered = [ones(n,1), gpa - mean(gpa)];
@@ -173,28 +135,42 @@ sigma_ncols = [3, 3];
 model_both = mom(X_centered, Y, Z_centered, sigma_ncols, true);
 clear Z_centered sigma_ncols
 
-% We can extract the standard error from the diagonal of the
-% variance-covariance matrix for B.
-model_both.SE = sqrt(diag(model_both.B_cov));
-
 % Un-center by adding the mean back the estimates of the fixed and random
 % effect intercepts.  Note this does *not* un-center the estimates of the
 % random effect sigmas.
 model_both.B(1) = model_both.B(1) - model_both.B(2).*mean(gpa);
 model_both.u(1:3) = model_both.u(1:3) - model_both.u(4:6).*mean(gpa);
 
-model_both.T = model_both.B ./ model_both.SE;
-
 % Scatterplot
 plot_clusters(model_both.B, model_both.u);
-title('Random Interecept + Slope, MoM');
+title('Mixed, Random Interecept + Slope (MoM)');
+
+% We can extract the standard error from the diagonal of the
+% variance-covariance matrix for B and compute the T values.
+model_both.SE = sqrt(diag(model_both.B_cov));
+model_both.T = model_both.B ./ model_both.SE;
 
 % Add to comparison table.
 comparison_tbl = [comparison_tbl; table(model_both.B', model_both.SE', 'VariableNames', {'B', 'SE'}, 'RowNames', {'MoM Intercept + Slope'})];
 
-%% Use REML, Random Intercept + Centered Slope
+%% Mixed Effects Model Using REML
+% With Random Intercept + Centered Slope for School
+
+% Note that we do not need to center the model when using REML, but we will
+% do so anyway for ease of comparison with the MoM parameters.
+
+% The syntax for fotlmematrix is a little different.  Instead of passing in
+% the actual random effects matrix Z, we pass in a pseudo-Z with one column
+% for each random effect.  For a random intercept+slope example, this Z
+% turns out to be the same as X!  We then specify the groupings in a
+% separate categorical variable, G.
 G = sum(school .* [1,2,3], 2);
 reml = fitlmematrix(X_centered,Y,X_centered,G, 'FitMethod', 'REML');
+
+% Extract the results.  Note that randomEffects() gives us the random
+% effects in a different order than above.  Intead of getting three random
+% effects for intercept and three for slope, we get three pairs of
+% [intercept, slope].
 model_reml = struct;
 model_reml.X = X_centered;
 model_reml.Y = Y;
@@ -204,6 +180,7 @@ model_reml.u = randomEffects(reml); model_reml.u = [model_reml.u(1); model_reml.
 model_reml.sigmas = diag(psi{1}); clear psi
 model_reml.mse = mse; clear mse
 model_reml.SE = sqrt(diag(reml.CoefficientCovariance));
+model_reml.T = model_reml.B ./ model_reml.SE;
 clear X_centered Y
 
 % Un-center by adding the mean back the estimates of the fixed and random
@@ -212,17 +189,34 @@ clear X_centered Y
 model_reml.B(1) = model_reml.B(1) - model_reml.B(2).*mean(gpa);
 model_reml.u(1:3) = model_reml.u(1:3) - model_reml.u(4:6).*mean(gpa);
 
-% Compute t-values.
-model_reml.T = model_reml.B ./ model_reml.SE;
-
 % Scatterplot
 plot_clusters(model_reml.B, model_reml.u);
-title('Random Intercept + Slope (REML)');
+title('Mixed, Random Intercept + Slope (REML)');
 
 % Add to the comparison table.
 comparison_tbl = [comparison_tbl; table(model_reml.B', model_reml.SE', 'VariableNames', {'B', 'SE'}, 'RowNames', {'REML Intercept + Slope'})];
 
 %% Fixed Effects with Clusters
+
+% We (obviously) cannot model a *random* effect of intercept and slope in a
+% fixed effect model.  Instead we will encode these as fixed effects.  Note
+% that in prior examples we specified two fixed effects columns and six
+% random effect columns (three for intercept, three for slope).  We cannot
+% do the same here because we would end up with a rank deficient design
+% matrix.  Instead, we model 6 fixed effect columns total.  The way we
+% encode cluster (school) membership is carefully chosen such that the
+% first two fixed effect columns will end up being the overall (marginal)
+% intercept and slope.
+%
+% In columns 3-4 of X, encoding the "random" intercept, there are three
+% schools to encode in, effectively, 2 columns.  The possible values of
+% each column must sum to zero.  For example, look at column 3.  Jennings
+% is encoded as 1.  Gateway and Burroughs is each encoded as -0.5. 
+% 1 - 0.5 - 0.5 = 0.  (It doesn't matter if the number of students in each
+% school balanced or not.)
+%
+% Columns 5-6 for "random" slope are simply the interaction of column 2
+% (marginal slope) with columns 3-4 ("random" interecept).
 
 X = [ones(n,1), gpa, school(:,1).*1.5-0.5, school(:,2).*1.5-0.5, gpa.*(school(:,1).*1.5-0.5), gpa.*(school(:,2).*1.5-0.5)];
 Y = sat;
@@ -234,12 +228,17 @@ u(3) = -0.5*B(3)-0.5*B(4);
 u(4) = B(5)-0.5*B(6);
 u(5) = B(6)-0.5*B(5);
 u(6) = -0.5*B(5)-0.5*B(6);
-% Find the standard errors.
-SE = sqrt(diag(pinv(X)*pinv(X)'*sum((Y-X*B).^2)/size(Y,1)));
-model_marginal_both = struct('X', X, 'Y', sat, 'B', B, 'u', u, 'SE', SE);
-clear X Y B u SE T
+model_marginal_both = struct('X', X, 'Y', sat, 'B', B, 'u', u);
 
-% Compute Standard Error for Fixed Effects Model Using Sandwich Estimator
+% Scatterplot
+plot_clusters(model_marginal_both.B, model_marginal_both.u);
+title('Fixed Effects With Fixed Clusters');
+
+% Standard error without sandwich estimator.
+model_marginal_both.SE = sqrt(diag(pinv(X)*pinv(X)'*sum((Y-X*B).^2)/size(Y,1)));
+clear X Y B u SE
+
+% Standard Error and T-values Using Sandwich Estimator
 Xpinv = pinv(model_marginal_both.X(:, 1:2));
 resid = model_marginal_both.Y - model_marginal_both.X(:, 1:2) * model_marginal_both.B(1:2);
 model_marginal_both.swe_covB = swe(Xpinv, resid, G);
@@ -248,13 +247,21 @@ model_marginal_both.SE_swe = sqrt(diag(model_marginal_both.swe_covB.block));
 model_marginal_both.T = model_marginal_both.B(1:2) ./ model_marginal_both.SE_swe(1:2);
 
 % Add to comparison table.
-comparison_tbl = [comparison_tbl; table(repmat(model_marginal_both.B(1:2)', 2, 1), [model_marginal_both.SE(1:2)'; sqrt(diag(model_marginal_both.swe_covB.block))'], 'VariableNames', {'B', 'SE'}, 'RowNames', {'Marginal Only', 'Marginal + Block SwE'})]
-
-% Scatterplot
-plot_clusters(model_marginal_both.B, model_marginal_both.u);
-title('Fixed Effects With Clusters (Marginal)');
+comparison_tbl = [comparison_tbl; table(repmat(model_marginal_both.B(1:2)', 2, 1), [model_marginal_both.SE(1:2)'; sqrt(diag(model_marginal_both.swe_covB.block))'], 'VariableNames', {'B', 'SE'}, 'RowNames', {'Marginal Only', 'Marginal + Block SwE'})];
 
 %% Visualize comparison table.
+
+% Observe that the fixed effects only (without clusters) estimate of the
+% intercept and slope is probably biased (assuming grade inflation is not
+% commented out at the top of this file).  This will happen anytime there
+% is a correlation between fixed effects and random effects design matrices
+% (in this case, gpa is correlated with which school a student attends.)
+%
+% Observe that, without using the sandwich estimator, the fixed effects
+% estimates of the standard error are much too narrow, which would lead to
+% inflated T-values and false positive results!  On the other hand, the SwE
+% SE tends to be bigger than the REML SE, leading to low efficiency and
+% false negative results.  The MoM SE tends to fall somewhere in between.
 
 % Fixed effects without clusters.
 B = comparison_tbl('Fixed Only', :).B(1); % estimate of interecept
@@ -304,7 +311,7 @@ boxdata_slope = boxdata_slope(:, [1 2 2 3 4 4 5])';
 f = figure; f.Position(3) = f.Position(3) * 2;
 subplot(1,2,1);
 boxplot(boxdata_int, 'Whisker', Inf);
-xlabels = {'Fixed Only', 'REML', 'MoM', 'Marginal'};
+xlabels = {'Fixed Only', 'Mixed REML', 'Mixed MoM', 'Fixed Clusters'};
 xticklabels(xlabels);
 ylabel('SAT Score');
 title('Interecept');
@@ -319,3 +326,58 @@ h_swe = h_box(2);
 h_se = h_box(5);
 legend([h_se, h_swe], 'Standard Error', 'SwE Error', 'Location', 'Northeast');
 clear h_box h_se h_swe  boxdata_slope xlabels f
+
+%% Additional Examples
+
+%% Mixed Effects Model, Random Intercept for School
+%{
+% The random effect design matrix is just three columns of ones and zeros
+% encoding which school the student goes to.
+Z = school;
+
+% There is only one random effect variance, that of the intercept.
+sigma_ncols = [3];
+
+% Fit the mixed effects model using the method of moments!
+model_int = mom(X, Y, Z, sigma_ncols);
+clear Z sigma_ncols;
+
+% We can extract the standard error from the diagonal of the
+% variance-covariance matrix for B.
+model_int.SE = sqrt(diag(model_int.B_cov));
+model_int.T = model_int.B ./ model_int.SE;
+
+% Scatterplot
+plot_clusters(model_int.B, [model_int.u; 0; 0; 0]);
+title('Random Interecept, MoM');
+
+% Add to comparison table.
+comparison_tbl = [comparison_tbl; table(model_int.B', model_int.SE', 'VariableNames', {'B', 'SE'}, 'RowNames', {'MoM Intercept'})];
+%}
+
+%% Mixed Effects Model, Random Slope for School
+%{
+
+% The random effect design matrix is three columns containing the GPA for
+% schools in the column, otherwise zero.
+Z = school.*gpa;
+
+% There is only one random effect variance, that of the slope.
+sigma_ncols = [3];
+
+% Fit the mixed effects model using the method of moments!
+model_slope = mom(X, Y, Z, sigma_ncols);
+clear X Z sigma_ncols;
+
+% We can extract the standard error from the diagonal of the
+% variance-covariance matrix for B.
+model_slope.SE = sqrt(diag(model_slope.B_cov));
+model_slope.T = model_slope.B ./ model_slope.SE;
+
+% Scatterplot
+plot_clusters(model_slope.B, [0; 0; 0; model_slope.u]);
+title('Random Slope, MoM');
+
+% Add to comparison table.
+comparison_tbl = [comparison_tbl; table(model_slope.B', model_slope.SE', 'VariableNames', {'B', 'SE'}, 'RowNames', {'MoM Slope'})];
+%}
